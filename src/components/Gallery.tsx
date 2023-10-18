1ba35234ffd01089cef087b4c55509eda7a5a635
jai <jai.bhalla2021@vitstudent.ac.in>
2023-10-18 21:49:53 +0530

    adding dark and light more

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card, Skeleton } from 'antd';
-import SkeletonCard from "./SkeletonCard";
-import Navbar from "./Navbar";
+import SkeletonCard from './SkeletonCard';
+import Navbar from './Navbar';
+import { AiOutlineLike } from 'react-icons/ai';
 
 const { Meta } = Card;
 
@@ -10,9 +11,10 @@ const Gallery = () => {
     const [data, setData] = useState<any>(null);
     const [loading, setLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState('');
+    const [theme, setTheme] = useState<'light' | 'dark'>('light');
     let baseUrl: string;
 
-    const REACT_APP_API_ACCESS_KEY = process.env.REACT_APP_API_ACCESS_KEY
+    const REACT_APP_API_ACCESS_KEY = process.env.REACT_APP_API_ACCESS_KEY;
 
     const fetchData1 = async () => {
         try {
@@ -34,66 +36,71 @@ const Gallery = () => {
     };
 
     useEffect(() => {
-        if(searchTerm !== ''){
+        if (searchTerm !== '') {
             baseUrl = `https://api.unsplash.com/search/photos/?client_id=${REACT_APP_API_ACCESS_KEY}&query=${searchTerm}&per_page=30`;
-            fetchData2()
-        }
-        else{
+            fetchData2();
+        } else {
             baseUrl = `https://api.unsplash.com/photos/?client_id=${REACT_APP_API_ACCESS_KEY}&per_page=30`;
             fetchData1();
         }
     }, [searchTerm]);
 
-    console.log(searchTerm);
-
     const handleSearch = (value: string) => {
         setSearchTerm(value);
     };
 
-    console.log(data);
+    const handleThemeChange = (newTheme: 'light' | 'dark') => {
+        setTheme(newTheme);
+    };
 
     return (
         <>
-            <Navbar searching={handleSearch}/>
-            <div className={'pt-20 px-20 flex w-full justify-center'}>
-                {
-                    loading ? (
-                        <div className={'columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 w-full mx-auto space-y-8 pb-28'}>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                            <SkeletonCard/>
-                        </div>
-                    ) : (
-                        <div className={'columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 w-full mx-auto space-y-8 pb-28'}>
-                            {Array.isArray(data) ? (
-                                data?.map((card: any) => (
-                                    <div key={card?.id} className={'hover:scale-110 transition-all duration-300 h-min m-2 break-inside-avoid flex justify-center'}>
-                                        <Card
-                                            hoverable={true}
-                                            style={{ width: 240 }}
-                                            cover={<img alt="example" src={card.urls.full} />}
-                                            className={'bg-[#141414] border-none'}
-                                        >
-                                            <div className={'text-white'}>
-                                                <p>{card?.sponsorship?.sponsor?.username}</p>
-                                                <p>{card?.alt_description} </p>
+            <Navbar searching={handleSearch} onThemeChange={handleThemeChange} />
+            <div className={`pt-10 px-4 md:px-20 flex w-full justify-center ${theme === 'dark' ? 'bg-[#101010]' : 'bg-gray-200'}`}>
+                {loading ? (
+                    <div className={'columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 w-full mx-auto space-y-8 pb-28'}>
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                    </div>
+                ) : (
+                    <div className={'columns-1 sm:columns-2 md:columns-3 lg:columns-4 gap-3 w-full mx-auto space-y-8 pb-28'}>
+                        {Array.isArray(data) ? (
+                            data?.map((card: any) => (
+                                <div key={card?.id} className={'hover:scale-110 transition-all duration-300 h-min m-2 break-inside-avoid flex justify-center'}>
+                                    <Card
+                                        hoverable={true}
+                                        style={{
+                                            width: '100%', // Set width to 100% for mobile view
+                                            maxWidth: '360px', // Set maximum width for larger screens
+                                            margin: 'auto', // Center the card
+                                        }}
+                                        cover={<img alt="example" src={card.urls.full} />}
+                                        className={`${theme === 'dark' ? 'bg-[#151515]' : 'bg-gray-300'} border-none`}
+                                    >
+                                        <div className={`${theme === 'dark' ? 'text-white' : 'text-black'}`}>
+                                            <p className={'text-xl font-bold'}>{card?.sponsorship?.sponsor?.username}</p>
+                                            <p className={'text-md font-normal'}>{card?.alt_description} </p>
+                                            <div className={'w-full flex justify-end gap-1 items-center'}>
+                                                {card?.likes}
+                                                <AiOutlineLike />
                                             </div>
-                                        </Card>
-                                    </div>
-                                ))
-                            ): (
-                                    <p>No array</p>
-                            )}
-                        </div>
-                    )
-                }
+                                        </div>
+                                    </Card>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No array</p>
+                        )}
+                    </div>
+                )}
             </div>
         </>
     );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Input, Switch } from 'antd';
 import { SearchProps } from 'antd/es/input';
 
@@ -6,14 +6,19 @@ const { Search } = Input;
 
 interface NavbarProps {
     searching?: SearchProps['onSearch'];
+    onThemeChange: (theme: 'light' | 'dark') => void;
 }
 
 const onSearch: SearchProps['onSearch'] = (value, _e, info) =>
     console.log(info?.source, value);
 
-const Navbar: React.FC<NavbarProps> = ({ searching = onSearch }) => {
+const Navbar: React.FC<NavbarProps> = ({ searching = onSearch, onThemeChange }) => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
     const onChange = (checked: boolean) => {
-        console.log(`switch to ${checked}`);
+        setIsDarkMode(checked);
+        const theme = checked ? 'dark' : 'light';
+        onThemeChange(theme);
     };
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -26,21 +31,41 @@ const Navbar: React.FC<NavbarProps> = ({ searching = onSearch }) => {
         }
     };
 
+    useEffect(() => {
+        // You can perform any additional actions when the theme changes
+        // For example, you can apply different CSS classes or update styles
+        document.body.className = isDarkMode ? 'dark-theme' : 'light-theme';
+    }, [isDarkMode]);
+
     return (
-        <nav className={'bg-[#232323] py-2 text-white px-20 flex justify-between w-full items-center'}>
-            <div className={'text-3xl font-thin'}>
-                FotoFolio
+        <>
+            <nav className={`${isDarkMode ? 'bg-[#232323]' : 'bg-gray-300'} py-2 text-white px-4 md:px-10 lg:px-20 flex justify-between w-full items-center`}>
+                <div className={`${isDarkMode ? 'text-white' : 'text-black'} select-none text-xl md:text-2xl lg:text-3xl font-thin`}>
+                    FotoFolio
+                </div>
+                <div className={'hidden md:flex'}>
+                    <Search
+                        placeholder="input search text"
+                        enterButton="Search"
+                        size="large"
+                        className={`w-96 rounded-md ${isDarkMode ? 'bg-[#2d2d2d]' : 'bg-gray-400'}`}
+                        onChange={handleSearch}
+                        onSearch={searching}
+                    />
+                </div>
+                <Switch defaultChecked={isDarkMode} onChange={onChange} />
+            </nav>
+            <div className={`${isDarkMode ? 'bg-[#232323]' : 'bg-gray-300'} flex items-cennter justify-center p-2 md:hidden`}>
+                <Search
+                    placeholder="input search text"
+                    enterButton="Search"
+                    size="large"
+                    className={'rounded-md bg-[#2d2d2d]'}
+                    onChange={handleSearch}
+                    onSearch={searching}
+                />
             </div>
-            <Search
-                placeholder="input search text"
-                enterButton="Search"
-                size="large"
-                className={'w-96 rounded-md bg-[#2d2d2d]'}
-                onChange={handleSearch}
-                onSearch={searching}
-            />
-            <Switch defaultChecked onChange={onChange} />
-        </nav>
+        </>
     );
 };
 
